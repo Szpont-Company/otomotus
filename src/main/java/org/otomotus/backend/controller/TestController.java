a103b7789b1b46bd4f447f34038b7c1956c39bf9
Splasz <splasz303@gmail.com>
2025-10-21 18:50:29 +0200

    Initial Commit

---
+++ src/main/java/org/otomotus/backend/controller/TestController.java
@@ -0,0 +1,14 @@
+package org.otomotus.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hello")
+public class TestController {
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
+}
