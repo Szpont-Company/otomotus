6a658541d0977a4b1bcb87708103737b50add4af
Splasz <splasz303@gmail.com>
2025-11-08 17:51:19 +0100

    feat: JWT filter and service

--- src/main/java/org/otomotus/backend/auth/config/SecurityConfig.java
+++ src/main/java/org/otomotus/backend/auth/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package org.otomotus.backend.config.security;
+package org.otomotus.backend.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
